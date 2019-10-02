7e81abafcd7fb73d70aa98d85e54a370e53d7ba0
InBrewJ <jason.brewer@arupiot.com>
2019-10-02 18:25:17 +0100

    arupiot/ishiki-brickd#2 Updated run command for brickd to work on the Pi Zero

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # ishiki-brickd Dockerfile
 
-docker run -it --rm --name brickd aupiot/ishiki-brickd:latest
+docker run -it -p 4223:4223 --privileged --device /dev/spidev0.0:/dev/spidev0.0 --rm --name brickd arupiot/ishiki-brickd:mini
