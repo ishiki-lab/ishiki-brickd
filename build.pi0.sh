ad2970544f52cddd91e10334e3807bf122e189f1
Francesco Anselmo <francesco.anselmo@gmail.com>
2019-11-17 00:53:46 +0000

    pi0 support added

---
+++ build.pi0.sh
@@ -0,0 +1,5 @@
+# ishiki-brickd Dockerfile
+
+sudo docker build -t arupiot/ishiki-brickd .
+sudo docker tag arupiot/ishiki-brickd arupiot/ishiki-brickd:pi0
+sudo docker push arupiot/ishiki-brickd:pi0
