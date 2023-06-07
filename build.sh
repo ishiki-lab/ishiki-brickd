60cc319487d7293ae53b507cf635dc4abd9c79e3
Francesco Anselmo <francesco.anselmo@gmail.com>
2023-06-08 00:20:37 +0900

    docker buildx

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 # ishiki-brickd Dockerfile
 
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t arupiot/ishiki-brickd:0.0.1 --push .
+docker buildx create --name ishikibuilder --use
+docker buildx use ishikibuilder
+docker buildx inspect --bootstrap
+
+docker buildx build --platform=linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t ishikilab/ishiki-brickd:0.0.1 -f Dockerfile.debian --push .
